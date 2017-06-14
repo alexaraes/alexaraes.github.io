c318dc4e4349a14b79ccc5810c52dc1c9027fc09
Alexa R Schreffler <alexa.schreffler@gmail.com>
2017-06-14 15:42:26 -0500

    teensy weensy functional homepage cuz why the hell not

--- src/App.js
+++ src/App.js
@@ -5,14 +5,15 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+      <div className="app">
+        <div className="header">
+          <h1>Alexa Rae Schreffler</h1>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="intro">
+          <p>This is a functional-ass website.</p>
+          <p>I am an Austin, TX based front end developer who really, really likes React.</p> 
+          <p>I try to keep my <a target="_blank" href="http://www.github.com/alexaraes">Github profile</a> active, say some stuff on my <a target="_blank" href="http://awrecksa.tumblr.com">Tumblr</a>, or post gifs on <a target="_blank" href="http://www.twitter.com/awrecksa">Twitter</a>.</p>
+        </div>
       </div>
     );
   }
